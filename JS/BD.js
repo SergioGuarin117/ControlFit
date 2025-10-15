ef6da95cc85ecd268218013446345e3f75b9fa7c
Sergio <sergioguarin117@gmail.com>
2025-10-15 14:45:56 -0500

    Sergio: Actualizo la autenticacion y mejoro el flujo del login

--- JS/BD.js
+++ JS/BD.js
@@ -13,22 +13,42 @@ request.onerror = (event) => {
 
 request.onupgradeneeded = (event) => {
     const db = event.target.result;
+    crearTabla(db);}
+function crearTabla(db) {
 
-    //Creamos las tablas a continuacion
+    if(!db.objectStoreNames.contains("Usuario")){
+        const usuario = db.createObjectStore("Usuario", {keypath: "id_usuario",autoIncrement: true})
 
-    const tablas = db.createObjectStore("Usuario", {
-        keypath: "id_usuario",
-        autoIncrement: true,
-    })
+        usuario.createIndex("nombre", "nombre", {unique:false}),
+        usuario.createIndex("email", "email", {unique:true}),
+        usuario.createIndex("usuario", "usuario", {unique:true}),
+        usuario.createIndex("password", "password", {unique:true}),
 
-    // Indices de texto
-    tablas.createIndex("nombre", "nombre", {unique:false});
-    tablas.createIndex("usuario", "usuario", {unique:false});
-    tablas.createIndex("password", "password", {unique:false});
+        usuario.createIndex("edad","edad", {unique:false});
+        usuario.createIndex("peso", "peso",{unique:false});
+        usuario.createIndex("altura", "altura", {unique:false});
+    }
 
-    tablas.createIndex("edad","edad", {unique:false});
-    tablas.createIndex("peso", "peso",{unique:false});
-    tablas.createIndex("altura", "altura", {unique:false});
+    if (!db.objectStoreNames.contains("Maquinas")){
+        const maquinas = db.createObjectStore("Maquinas", {keypath: "id_maquina",autoIncrement: true})
+        maquinas.createIndex("nombreMaquina", "nombreMaquina", {unique:false}),
+        maquinas.createIndex("musculacion", "musculacion", {unique:false}, Boolean),
+        maquinas.createIndex("cardio", "cardio", {unique:false}, Boolean),
+        maquinas.createIndex("bajarPeso", "bajarPeso", {unique:false});
+    }
 
-    console.log("Tabla USUARIO creada correctamente");
+    if (!db.objectStoreNames.contains("Ejercicios")){
+        const ejercicios = db.createObjectStore("Ejercicios", {keypath: "id_ejercicio",autoIncrement: true})
+        ejercicios.createIndex("id_maquinas_fk", "id_maquinas", {unique:false}),
+        ejercicios.createIndex("nombreEjercicio", "nombreEjercicio", {unique:false}),
+        ejercicios.createIndex("repeticiones", "repeticiones", {unique:false}),
+        ejercicios.createIndex("descripcion","descripcion", {unique:false});
+    }
+
+    if (!db.objectStoreNames.contains("Rutinas")){
+        const rutinas = db.createObjectStore("Rutinas", {keypath: "id_rutina",autoIncrement: true})
+        rutinas.createIndex("id_ejercicios_fk", "id_ejercicios", {unique:false}),
+        rutinas.createIndex("id_usuario_fk", "id_usuario", {unique:false}),
+        rutinas.createIndex("fecha", "fecha", {unique:false}, Date);
+    }
 }
